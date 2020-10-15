5d22d325eb8a98da7ef8857738f7b583ce2c5174
ztonyz1212 <ztonyz@126.com>
2020-10-15 13:54:44 +0800

    json

--- projects/haier/json.sql
+++ projects/haier/json.sql
@@ -4,3 +4,19 @@ from meihao_self_credit_request
 where request_type = 'acard'
 order by create_time desc
 limit 100
+
+/* 全数值格式
+'td_6m_apply_p2p_cnt',
+'td_idcard_3m_contact_phone_cnt',
+'consumer_apply_amount',
+'td_3m_apply_bank_xiaojin_cnt',
+'td_idcard_middle_risk_list',
+'alipay_credit_score',
+'td_6m_apply_m_xiaojin_cnt',
+'td_6m_apply_microloan_cnt',
+'month_count',
+'final_score',
+'td_idcard_low_risk_list',
+'bj_score',
+'age'
+*/
